6f4a99f66921713000964deded54243e0570a150
buaasniper <buaasniper@gmail.com>
2019-07-12 04:16:54 -0400

    28000

--- aquarium/aquarium.js
+++ aquarium/aquarium.js
@@ -46,7 +46,7 @@ var g_numSettingElements = {};
 var g_scenes = {};  // each of the models
 var g_sceneGroups = {};  // the placement of the models
 var g_fog = true;
-var g_numFish = [1, 100, 26000, 1000, 5000, 10000, 15000, 20000, 25000, 30000];
+var g_numFish = [1, 100, 28000, 1000, 5000, 10000, 15000, 20000, 25000, 30000];
 
 var g_stereoDemoActive = false;
 var g_shadersNeedUpdate = false; // Set to true whenever the state has changed so that shaders may need to be changed.
